fb06fbc4a8038797b7e7af10e9a31008fdb66f66
Maxime Gélinas <mgelinas@softframe.net>
2020-11-23 10:21:42 -0500

    feat: bump version to 1.3.0

--- packages/foo/src/hello.ts
+++ packages/foo/src/hello.ts
@@ -1,2 +1,2 @@
 export const hello = (...dependencies: string[]): void =>
-  console.log(`hello from v1.x using:\n${['foo', ...dependencies].map(dep => `  - ${dep}`).join('\n')}`);
+  console.log(`hello from v1.3.x using:\n${['foo', ...dependencies].map(dep => `  - ${dep}`).join('\n')}`);
